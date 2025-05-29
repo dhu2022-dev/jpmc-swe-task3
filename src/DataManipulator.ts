7763cfaff2114c0ea21e56662bb782f8012690e2
David Hu <who.is.david101@gmail.com>
2025-05-29 12:30:36 -0400

    Fix "trigger alert" display

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -16,8 +16,8 @@ export class DataManipulator {
     const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
     const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
-    const upperBound = 1 + 0.1;
-    const lowerBound = 1 - 0.1;
+    const upperBound = 1+0.025;//1 + 0.1;
+    const lowerBound = 1-0.025;//1 - 0.1;
     return {
       price_abc: priceABC,
       price_def: priceDEF,
@@ -26,7 +26,7 @@ export class DataManipulator {
         serverResponds[0].timestamp : serverResponds[1].timestamp,
       upper_bound: upperBound,
       lower_bound: lowerBound,
-      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? 1.1 : undefined
     };
   }
 }
